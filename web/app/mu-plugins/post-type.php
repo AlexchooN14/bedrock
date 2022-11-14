271f8693d06df93fdb5cb06b9db9ab1425f5ec08
AlexchooN14 <alexandern003@gmail.com>
2022-11-14 18:50:40 +0200

    Biotrade Bundle

---
+++ web/app/mu-plugins/post-type.php
@@ -0,0 +1,4 @@
+<?php 
+    require_once(dirname(__FILE__) . '/post-type/FAQ.php');
+    require_once(dirname(__FILE__) . '/post-type/Bundle.php');
+    require_once(dirname(__FILE__) . '/post-type/BadIngredients.php');
